0e4d78c2d30bd3ff63a6d270e146d6d9d67a22a2
Andre Carvalho <asantostc@gmail.com>
2024-09-21 19:20:58 +0100

    configure undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,12 @@
 return {
   "mbbill/undotree",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>uu", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+  },
   config = function()
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    vim.g.undotree_WindowLayout = 3
+    vim.g.undotree_SplitWidth = 50
+    vim.g.undotree_SetFocusWhenToggle = 1
   end,
 }
