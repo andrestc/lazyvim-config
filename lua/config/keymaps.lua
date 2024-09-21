a8f58d9569768d856fba9efbc9eb8af5b764f7ff
Andre Carvalho <asantostc@gmail.com>
2024-09-21 19:54:25 +0100

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,18 @@ map("n", "<Up>", "<C-w><C-k>", { noremap = true })
 map("n", "<Down>", "<C-w><C-j>", { noremap = true })
 map("n", "<Left>", "<C-w><C-h>", { noremap = true })
 map("n", "<Right>", "<C-w><C-l>", { noremap = true })
+
+-- allow "moving" the selection up and down
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("n", "J", "mzJ`z")
+
+-- Moving
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Inline paste on visual mode
+map("x", "<leader>p", [["_dP]])
+
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
