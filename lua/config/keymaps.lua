61ffc5bf53c30f78e7326e25cbc783df644ee76b
Andre Carvalho <asantostc@gmail.com>
2024-09-21 14:18:56 +0100

    arrows and cleanups

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,9 @@ local map = vim.keymap.set
 -- Open terminal on a vertical split windows (vscode like)
 -- nmap <C-S-P> :split | terminal <CR>
 map("n", "<Leader>t", "<Cmd>split | terminal <CR>", { desc = "Open terminal on split window" })
+
+-- map arrow keys to pane shift
+map("n", "<Up>", "<C-w><C-k>", { noremap = true })
+map("n", "<Down>", "<C-w><C-j>", { noremap = true })
+map("n", "<Left>", "<C-w><C-h>", { noremap = true })
+map("n", "<Right>", "<C-w><C-l>", { noremap = true })
